44d0a1f9fd2a0c8002e9105b1f3940bc00d5a3e1
exigua <o.selcuk@it-students.fr>
2019-10-24 17:32:55 +0200

    first commit

---
+++ orkun.js
@@ -0,0 +1,17 @@
+function popUp() {
+    alert("De nouveaux modèles arriveront bientôt.");
+}
+function popUp2() {
+    alert("De nouveaux modèles arriveront bientôt.");
+}
+
+let colorful = document.querySelector(".colorful");
+
+colorful.addEventListener("click", () => {
+    document.querySelector("body");
+    document.querySelector("body").classList.toggle("body1");
+
+});
+
+
+    
